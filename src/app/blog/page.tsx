70635194a488f0b3a439e04ab5a6e5a959863bbc
efeckgz <efeckgz@gmail.com>
2024-11-04 00:16:57 +0300

    Working on blog mockup

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,56 @@
-export default function Blog() {
-  return (
-    <div>
-      <h1>The blog page</h1>
-    </div>
-  );
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+interface BlogPreview {
+  title: string;
+  contents: string;
 }
+
+const previews: BlogPreview[] = [
+  {
+    title: "Lorem Ipsum",
+    contents:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    title: "Lorem Ipsum",
+    contents:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+  {
+    title: "Lorem Ipsum",
+    contents:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  },
+];
+
+const Blog = () => {
+  return (
+    <>
+      {previews.map((p) => {
+        return <BlogPreviewCard title={p.title} contents={p.contents} />;
+      })}
+    </>
+  );
+};
+
+const BlogPreviewCard = ({ title, contents }: BlogPreview) => {
+  return (
+    <Card className="m-5">
+      <CardHeader className="text-4xl">
+        <CardTitle>{title}</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p>{contents}</p>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Blog;
