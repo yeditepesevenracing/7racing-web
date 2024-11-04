ddb060de364b3360e33b10669bfbac252062bf51
efeckgz <efeckgz@gmail.com>
2024-11-04 21:00:17 +0300

    Implemented a basic blog screen.

--- src/app/blogpost/[id]/page.tsx
+++ src/app/blogpost/[id]/page.tsx
@@ -2,13 +2,17 @@ import Title from "@/components/common/title";
 import Paragraph from "@/components/common/paragraph";
 
 const BlogPost = async ({ params }: { params: Promise<{ id: number }> }) => {
+  // Fetch the blog post from the server using the id and then show its contents
   const id = (await params).id;
+
   const dummy =
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
   return (
-    <div className="flex flex-col">
-      <Title text="Lorem Ipsum" className="w-full" />
+    <div className="flex flex-col items-center">
+      <div>
+        <Title text="Lorem Ipsum" className="items-center" />
+      </div>
       <Paragraph>{dummy}</Paragraph>
     </div>
   );
--- src/components/common/paragraph.tsx
+++ src/components/common/paragraph.tsx
@@ -10,7 +10,7 @@ const Paragraph = ({
   return (
     <p
       className={cn(
-        "mt-4 text-2xl mx-40 mb-3 first-letter:text-7xl first-letter:font-bold first-letter:text-black-900 first-letter:me-3 first-letter:float-start",
+        "mt-4 text-2xl mx-60 mb-3 first-letter:text-8xl first-letter:font-bold first-letter:text-black-900 first-letter:me-3 first-letter:float-start",
         className
       )}
     >
