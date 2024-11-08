100e5be30b05d70b47a9c0100f4913c9397ca044
efeckgz <efeckgz@gmail.com>
2024-11-08 21:02:17 +0300

    Added a simple gradient background. Needs work.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -128,6 +128,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Menubar />
+        <div className="absolute inset-0 -z-10 h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#269b76_100%)]"></div>
         <div className="pt-[64px] overflow-auto">{children}</div>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <Title text="We are 7Racing." />
-      <p>The about section goes inside here.</p>
+      <Title text="We are 7Racing." className="items-center" />
     </div>
   );
 }
