59f65c0dbedf09b0894e6fe14ddc39b4949006e4
efeckgz <efeckgz@gmail.com>
2024-11-09 03:12:32 +0300

    Moved gradient background to home screen. Did some stylin'.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <Title text="We are 7Racing." className="items-center" />
+      <div className="absolute inset-0 -z-10 h-full w-full bg-white [background:radial-gradient(125%_125%_at_50%_10%,#fff_40%,#269b76_100%)]"></div>
+      <Title text="We are 7Racing." className="flex justify-center mt-40" />
+      <p className="flex justify-center mt-20 text-xl text-gray">
+        Fuelled by our passion for engineering, working towards Shell Eco
+        Marathon 2025.
+      </p>
     </div>
   );
 }
